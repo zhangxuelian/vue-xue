037af5a7e2438c1a615c04622c2189291cfb84f0
zhangxuelian <761303248@qq.com>
2020-09-18 16:42:21 +0800

    feat: zxl add demo HMR、

--- build/demo.js
+++ build/demo.js
@@ -1,22 +1,25 @@
 'use strict';
 
+const path = require('path');
 const webpack = require('webpack');
+const WebpackDevServer = require('webpack-dev-server');
 const demoConfig = require('./demo.config');
 
 demoConfig.mode = 'development';
 
-webpack(demoConfig, (err, stats) => {
-    if (err) {
-        console.error(err.stack || err);
-    } else if (stats.hasErrors()) {
-        console.error(stats.toString({
-            chunks: false,
-            colors: true
-        }));
-    } else {
-        console.log(stats.toString({
-            chunk: false,
-            colors: true
-        }));
+var compiler = webpack(demoConfig);
+compiler.hooks.watchRun.tapAsync('watch-run', (compilation, done) => {
+    console.log('demo process is compiling...');
+    done();
+});
+compiler.hooks.done.tap('done', stats => {
+    console.log('demo process compiled done.');
+});
+var server = new WebpackDevServer(compiler, {
+    open: true,
+    contentBase: path.join(__dirname, '../'),
+    after() {
+        // doto
     }
 });
+server.listen(8787);
